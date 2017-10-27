d5fab62d569908d6f88101a16bd24e95f97725a7
Anish Timila <anish.timila@ttu.edu>
2017-10-26 21:51:08 -0500

    Added files.

---
+++ app/src/main/java/com/topa/techpark/MainActivity.java
@@ -0,0 +1,44 @@
+package com.topa.techpark;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button findPark = (Button)findViewById(R.id.button_find_park);
+        findPark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, find_parking.class);
+                startActivity(intent);
+            }
+        });
+
+        Button niteShuttle = (Button)findViewById(R.id.button_nite);
+        niteShuttle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, nite_shuttle.class);
+                startActivity(intent);
+            }
+        });
+
+        Button safeRide = (Button)findViewById(R.id.button_safe);
+        safeRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, safe_ride.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
